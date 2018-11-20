53ed8491376a7b676c99d4057af3fa17f6a9da80
andrecarrsd2008 <andrecarrsd2008@gmail.com>
2018-11-19 20:49:27 -0800

    Added static folder & prepared SPA

--- server/index.js
+++ server/index.js
@@ -11,7 +11,16 @@ app.use(cors());
 
 const posts = require('./routes/api/posts');
 
-app.use('/api/posts', posts);
+app.use('/api/posts', posts); 
+
+// Handle production
+if(process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+   // Hand SPA
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 5000;
 
